06f1e18e274eb5409dcc4011dbd8df49be43c540
nandhuzz <anandhu7299@gmail.com>
2020-08-04 16:53:47 +0530

    Updates

---
+++ precache-manifest.e5b537eab3561c0fc4f829ec4f8223ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6e852231a650e9163b404674cd9328",
+    "url": "/tik-tak-to/index.html"
+  },
+  {
+    "revision": "ba45ba6911e80d69c21f",
+    "url": "/tik-tak-to/static/css/main.415ae669.chunk.css"
+  },
+  {
+    "revision": "e0d55fcbbde82779b61a",
+    "url": "/tik-tak-to/static/js/2.d24b332d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tik-tak-to/static/js/2.d24b332d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba45ba6911e80d69c21f",
+    "url": "/tik-tak-to/static/js/main.9af040e8.chunk.js"
+  },
+  {
+    "revision": "807d6ae0d6d6ed4f02b6",
+    "url": "/tik-tak-to/static/js/runtime-main.914954fa.js"
+  }
+]);
